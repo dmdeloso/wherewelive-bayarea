8b67f76bdc9b3e53d6725250cf54ef388834ec4f
David Deloso <daviddeloso@nuit-50.44mont.gvad.net>
2022-01-03 17:13:14 -0800

    initial commit

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiZG1kZWxvc28iLCJhIjoiY2trMGZ6aXJhMDVqdDJvbnI4YzM5MHRraiJ9.nOwnc18LqvUNErSlg4N0AA'}).addTo(map);
 
 let mapList = [
-    [37.78081, -122.44995, 'David, Jordan, Max', '310 Anza St'], [37.76783, -122.44586, 'Agnes, Skye, Lynn', '73 Delmar Street'],[37.79186, -122.40849, 'Ilana', '851 California Street'], [37.78847, -122.43075, 'Josh, Kevin, Charlie, Joshua', '2112a Pine St'], [37.79414, -122.41324, 'Maren, Sophie, Sooim', '1255 Taylor St'], [36.20209, -119.35232, 'Rawan, Tina, Jess, Lauren', '447 Greent St']
+    [37.78081, -122.44995, 'David, Jordan, Max', '310 Anza St'], [37.76783, -122.44586, 'Agnes, Skye, Lynn', '73 Delmar Street'],[37.79186, -122.40849, 'Ilana', '851 California Street'], [37.78847, -122.43075, 'Josh, Kevin, Charlie, Joshua', '2112a Pine St'], [37.79414, -122.41324, 'Maren, Sophie, Sooim', '1255 Taylor St'], [37.799620, -122.406520, 'Rawan, Tina, Jess, Lauren', '447 Greent St', [37.799360, -122.404790, 'Maria, Kim, Meher', '440 Vallejo St.']]
 
 ]
 
